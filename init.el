a01be3808b58cd93a8c3e8483e004216767c25bc
superhans2 <ashughes001@gmail.com>
2024-03-17 17:41:28 +0000

    init commit

--- init.el
+++ init.el
@@ -1,16 +1,14 @@
 ;; following Systems Crafters config
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
-
 ;; Profile emacs startup
 (add-hook 'emacs-startup-hook
-          (lambda ()
-            (message "*** Emacs loaded in %s with %d garbage collections."
-                     (format "%.2f seconds"
-                             (float-time
-                              (time-subtract after-init-time before-init-time)))
-                     gcs-done)))
-
+	  (lambda ()
+	    (message "*** Emacs loaded in %s with %d garbage collections."
+		     (format "%.2f seconds"
+			     (float-time
+			      (time-subtract after-init-time before-init-time)))
+		     gcs-done)))
 
 
 
@@ -18,9 +16,9 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 ;; (unless package-archive-contents
@@ -49,14 +47,6 @@
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-
-;; (use-package openwith
-;;   :ensure t
-;;   :config
-;;   (openwith-mode t)
-;;   (setq openwith-associations '(("\\.pdf\\'" "okular" (file)))))
-
-
 ;; Thanks, but no thanks
 (setq inhibit-startup-message t)
 
@@ -81,23 +71,23 @@
   :defer t
   :config
   (setq org-journal-dir "~/org/journal/"
-        org-journal-file-type 'weekly
-        org-journal-file-format "%Y-%m-%d.org"
-        org-journal-enable-agenda-integration t
-        org-extend-today-until 4
-        org-journal-date-format "%a, %Y-%m-%d"))
+	org-journal-file-type 'weekly
+	org-journal-file-format "%Y-%m-%d.org"
+	org-journal-enable-agenda-integration t
+	org-extend-today-until 4
+	org-journal-date-format "%a, %Y-%m-%d"))
 
 
 (defun dw/evil-hook ()
   (dolist (mode '(custom-mode
-                  eshell-mode
-                  git-rebase-mode
-                  erc-mode
-                  circe-server-mode
-                  circe-chat-mode
-                  circe-query-mode
-                  sauron-mode
-                  term-mode))
+		  eshell-mode
+		  git-rebase-mode
+		  erc-mode
+		  circe-server-mode
+		  circe-chat-mode
+		  circe-query-mode
+		  sauron-mode
+		  term-mode))
   (add-to-list 'evil-emacs-state-modes mode)))
 
 
@@ -132,7 +122,7 @@
   (evil-collection-outline-bind-tab-p nil)
   :config
   (setq evil-collection-mode-list
-        (remove 'lispy evil-collection-mode-list))
+	(remove 'lispy evil-collection-mode-list))
   (evil-collection-init))
 
 
@@ -144,17 +134,66 @@
 (keymap-global-set "C-c j" 'org-journal-new-entry)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+(use-package vertico
+  :ensure t
+  :bind (:map vertico-map
+	 ("C-j" . vertico-next)
+	 ("C-k" . vertico-previous)
+	 ("C-f" . vertico-exit)
+	 :map minibuffer-local-map
+	 ("M-h" . backward-kill-word))
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+
+(use-package marginalia
+  :after vertico
+  :ensure t
+  :custom
+  (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+  :init
+  (marginalia-mode))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out"))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("/home/alex/org/journal/2023-10-02.org"))
- '(package-selected-packages '(olivetti ## org-journal))
+ '(menu-bar-mode nil)
+ '(org-agenda-files '("/home/alex/org/journal/2024-03-04.org"))
+ '(package-selected-packages
+   '(which-key vertico undo-tree perspective org-journal olivetti marginalia hydra evil-collection doom-modeline))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "JetBrains Mono" :foundry "JB" :slant normal :weight light :height 128 :width normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight regular :height 158 :width normal)))))
+
+(use-package perspective
+  :ensure t  ; use `:straight t` if using straight.el!
+  :bind
+  (("C-x C-b" . persp-list-buffers))
+  :custom
+  (persp-mode-prefix-key (kbd "C-x x"))
+  :init
+  (persp-mode))
