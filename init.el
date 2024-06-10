ea674b712cd16fcc7c377dfa0cbf372d2bd35255
superhans2 <ashughes001@gmail.com>
2024-06-10 13:10:14 +0100

    copied code for setting up tab bars

--- init.el
+++ init.el
@@ -368,3 +368,37 @@
 	  (lambda ()
 	    (hs-minor-mode)))
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+
+;; copying tab bar navigation code 
+(defhydra mmk2410/tab-bar (:color teal)
+  "My tab-bar helpers"
+  ("a" mmk2410/tab-bar-run-agenda "Agenda")
+  ("j" mmk2410/tab-bar-run-journal "Journal"))
+
+(global-set-key (kbd "C-c f") 'mmk2410/tab-bar/body)
+
+(defun mmk2410/tab-bar-switch-or-create (name func)
+  (if (mmk2410/tab-bar-tab-exists name)
+      (tab-bar-switch-to-tab name)
+    (mmk2410/tab-bar-new-tab name func)))
+
+(defun mmk2410/tab-bar-tab-exists (name)
+  (member name
+          (mapcar #'(lambda (tab) (alist-get 'name tab))
+                  (tab-bar-tabs))))
+
+(defun mmk2410/tab-bar-new-tab (name func)
+  (when (eq nil tab-bar-mode)
+    (tab-bar-mode))
+  (tab-bar-new-tab)
+  (tab-bar-rename-tab name)
+  (funcall func))
+
+(defun mmk2410/tab-bar-run-journal ()
+  (interactive)
+  (mmk2410/tab-bar-switch-or-create
+   "Journal"
+   #'org-journal-open-current-journal-file))
+
+;; emacs startup hook
+(add-hook 'emacs-startup-hook 'mmk2410/tab-bar-run-journal)
