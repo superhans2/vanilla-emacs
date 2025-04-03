82400d57f7b3166e47cff7525076acc9cb4a63a0
superhans2 <ashughes001@gmail.com>
2025-04-03 11:43:02 +0100

    changes to org mode

--- init.el
+++ init.el
@@ -80,6 +80,7 @@
   (setq bookmark-save-flag 1)
   (setq inhibit-startup-message t)
   (setq make-backup-files nil)
+  (cua-mode)
   (auto-save-mode -1)
   (scroll-bar-mode -1)
   (tool-bar-mode -1)
@@ -385,6 +386,7 @@
 
 
 ;;; ORG 
+;;;; general 
 
 (use-package org
   :ensure t
@@ -443,6 +445,152 @@
     (add-hook 'before-save-hook 'delete-trailing-whitespace nil t))
   (add-hook 'org-mode-hook 'my-org-mode-setup)
 
+;;;; dwim
+(defun +org/dwim-at-point (&optional arg)
+    "Do-what-I-mean at point.
+
+If on a:
+- checkbox list item or todo heading: toggle it.
+- citation: follow it
+- headline: cycle ARCHIVE subtrees, toggle latex fragments and inline images in
+  subtree; update statistics cookies/checkboxes and ToCs.
+- clock: update its time.
+- footnote reference: jump to the footnote's definition
+- footnote definition: jump to the first reference of this footnote
+- timestamp: open an agenda view for the time-stamp date/range at point.
+- table-row or a TBLFM: recalculate the table's formulas
+- table-cell: clear it and go into insert mode. If this is a formula cell,
+  recaluclate it instead.
+- babel-call: execute the source block
+- statistics-cookie: update it.
+- src block: execute it
+- latex fragment: toggle it.
+- link: follow it
+- otherwise, refresh all inline images in current tree."
+    (interactive "P")
+    (if (button-at (point))
+	(call-interactively #'push-button)
+      (let* ((context (org-element-context))
+             (type (org-element-type context)))
+	;; skip over unimportant contexts
+	(while (and context (memq type '(verbatim code bold italic underline strike-through subscript superscript)))
+          (setq context (org-element-property :parent context)
+		type (org-element-type context)))
+	(pcase type
+          ((or `citation `citation-reference)
+           (org-cite-follow context arg))
+
+          (`headline
+           (cond ((memq (bound-and-true-p org-goto-map)
+			(current-active-maps))
+                  (org-goto-ret))
+		 ((and (fboundp 'toc-org-insert-toc)
+                       (member "TOC" (org-get-tags)))
+                  (toc-org-insert-toc)
+                  (message "Updating table of contents"))
+		 ((string= "ARCHIVE" (car-safe (org-get-tags)))
+                  (org-force-cycle-archived))
+		 ((or (org-element-property :todo-type context)
+                      (org-element-property :scheduled context))
+                  (org-todo
+                   (if (eq (org-element-property :todo-type context) 'done)
+                       (or (car (+org-get-todo-keywords-for (org-element-property :todo-keyword context)))
+                           'todo)
+                     'done))))
+           ;; Update any metadata or inline previews in this subtree
+           (org-update-checkbox-count)
+           (org-update-parent-todo-statistics)
+           (when (and (fboundp 'toc-org-insert-toc)
+                      (member "TOC" (org-get-tags)))
+             (toc-org-insert-toc)
+             (message "Updating table of contents"))
+           (let* ((beg (if (org-before-first-heading-p)
+                           (line-beginning-position)
+			 (save-excursion (org-back-to-heading) (point))))
+                  (end (if (org-before-first-heading-p)
+                           (line-end-position)
+			 (save-excursion (org-end-of-subtree) (point))))
+                  (overlays (ignore-errors (overlays-in beg end)))
+                  (latex-overlays
+                   (cl-find-if (lambda (o) (eq (overlay-get o 'org-overlay-type) 'org-latex-overlay))
+                               overlays))
+                  (image-overlays
+                   (cl-find-if (lambda (o) (overlay-get o 'org-image-overlay))
+                               overlays)))
+             (+org--toggle-inline-images-in-subtree beg end)
+             (if (or image-overlays latex-overlays)
+		 (org-clear-latex-preview beg end)
+               (org--latex-preview-region beg end))))
+
+          (`clock (org-clock-update-time-maybe))
+
+          (`footnote-reference
+           (org-footnote-goto-definition (org-element-property :label context)))
+
+          (`footnote-definition
+           (org-footnote-goto-previous-reference (org-element-property :label context)))
+
+          ((or `planning `timestamp)
+           (org-follow-timestamp-link))
+
+          ((or `table `table-row)
+           (if (org-at-TBLFM-p)
+               (org-table-calc-current-TBLFM)
+             (ignore-errors
+               (save-excursion
+		 (goto-char (org-element-property :contents-begin context))
+		 (org-call-with-arg 'org-table-recalculate (or arg t))))))
+
+          (`table-cell
+           (org-table-blank-field)
+           (org-table-recalculate arg)
+           (when (and (string-empty-p (string-trim (org-table-get-field)))
+                      (bound-and-true-p evil-local-mode))
+             (evil-change-state 'insert)))
+
+          (`babel-call
+           (org-babel-lob-execute-maybe))
+
+          (`statistics-cookie
+           (save-excursion (org-update-statistics-cookies arg)))
+
+          ((or `src-block `inline-src-block)
+           (org-babel-execute-src-block arg))
+
+          ((or `latex-fragment `latex-environment)
+           (org-latex-preview arg))
+
+          (`link
+           (let* ((lineage (org-element-lineage context '(link) t))
+                  (path (org-element-property :path lineage)))
+             (if (or (equal (org-element-property :type lineage) "img")
+                     (and path (image-type-from-file-name path)))
+		 (+org--toggle-inline-images-in-subtree
+                  (org-element-property :begin lineage)
+                  (org-element-property :end lineage))
+               (org-open-at-point arg))))
+
+          ((guard (org-element-property :checkbox (org-element-lineage context '(item) t)))
+           (org-toggle-checkbox))
+
+          (`paragraph
+           (+org--toggle-inline-images-in-subtree))
+
+          (_
+           (if (or (org-in-regexp org-ts-regexp-both nil t)
+                   (org-in-regexp org-tsr-regexp-both nil  t)
+                   (org-in-regexp org-link-any-re nil t))
+               (call-interactively #'org-open-at-point)
+             (+org--toggle-inline-images-in-subtree
+              (org-element-property :begin context)
+              (org-element-property :end context))))))))
+
+(general-define-key
+ :states '(normal)
+ :keymaps 'org-mode-map
+ "RET" #'+org/dwim-at-point)
+
+
 ;;;; beautifying org-mode
   ;; use org-bullets
   (require 'org-bullets)
@@ -465,6 +613,7 @@
 (use-package org-bullets)
 
   
+
 ;;;; org journal
 
 (use-package org-journal
@@ -474,6 +623,7 @@
 	org-journal-file-format "%Y-%m.org"
 	org-journal-enable-agenda-integration nil    ;; TODO change this at some point
 	org-extend-today-until 4
+	org-journal-carryover-items ""
 	org-journal-date-format "%a, %Y-%m-%d"
 	;; org-journal-enable-encryption t
 	org-journal-find-file #'find-file)
@@ -491,18 +641,10 @@
     (interactive)
     (let ((current-prefix-arg '(4)))  ;; Simulate C-u
       (call-interactively 'org-journal-new-entry))
-    (call-interactively 'outline-up-heading)
+      (call-interactively 'outline-up-heading)
     (call-interactively 'org-narrow-to-subtree)
     )
-  (baz/org-journal-narrow-today)
-
-  ;; (defun baz/org-journal-new-entry()
-  ;;   """ copying org-journal functionality for adding new journal entry"""
-  ;;   (interactive)
-  ;;   (let* (now (decode-time nil))
-  ;;     )
-    
-  ;;   )
+  ;; (baz/org-journal-narrow-today)
 
 
   :general
